90a769bbd59776fc6e87c3556362c1c351f8680b
Akena <hubforroblox@gmail.com>
2026-05-11 20:01:32 -0400

    Update command descriptions and add settings command

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,12 +3,34 @@ const { REST, Routes, SlashCommandBuilder, PermissionFlagsBits } = require('disc
 const commands = [
     new SlashCommandBuilder()
         .setName('startprotecting')
-        .setDescription('Enables invite spam and hacked account detection.')
+        .setDescription('Enables the master protection system.')
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
     new SlashCommandBuilder()
         .setName('stopprotecting')
-        .setDescription('Disables invite spam and hacked account detection.')
+        .setDescription('Disables the master protection system.')
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    new SlashCommandBuilder()
+        .setName('settings')
+        .setDescription('Configure protection settings')
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+        .addSubcommand(subcommand =>
+            subcommand.setName('links')
+            .setDescription('Configure Discord invite protection')
+            .addBooleanOption(opt => opt.setName('enabled').setDescription('Enable or disable link protection').setRequired(true))
+            .addIntegerOption(opt => opt.setName('timeout').setDescription('Timeout duration in minutes').setRequired(false))
+        )
+        .addSubcommand(subcommand =>
+            subcommand.setName('images')
+            .setDescription('Configure hacked account (image spam) protection')
+            .addBooleanOption(opt => opt.setName('enabled').setDescription('Enable or disable image protection').setRequired(true))
+            .addIntegerOption(opt => opt.setName('max_amount').setDescription('Max images allowed per message').setRequired(false))
+            .addIntegerOption(opt => opt.setName('timeout').setDescription('Timeout duration in minutes').setRequired(false))
+        )
+        .addSubcommand(subcommand =>
+            subcommand.setName('logs')
+            .setDescription('Set the channel where protection logs are sent')
+            .addChannelOption(opt => opt.setName('channel').setDescription('The channel to send logs to').setRequired(true))
+        )
 ].map(command => command.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
