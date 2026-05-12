80cde3ce2983394fcc50af514deb2c6e7fca8965
Akena <hubforroblox@gmail.com>
2026-05-11 23:08:38 -0400

    Update settings command description and remove subcommands

--- deploy-commands.js
+++ deploy-commands.js
@@ -11,26 +11,8 @@ const commands = [
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
     new SlashCommandBuilder()
         .setName('settings')
-        .setDescription('Configure protection settings')
+        .setDescription('Opens the modern protection configuration dashboard.')
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-        .addSubcommand(subcommand =>
-            subcommand.setName('links')
-            .setDescription('Configure Discord invite protection')
-            .addBooleanOption(opt => opt.setName('enabled').setDescription('Enable or disable link protection').setRequired(true))
-            .addIntegerOption(opt => opt.setName('timeout').setDescription('Timeout duration in minutes').setRequired(false))
-        )
-        .addSubcommand(subcommand =>
-            subcommand.setName('images')
-            .setDescription('Configure hacked account (image spam) protection')
-            .addBooleanOption(opt => opt.setName('enabled').setDescription('Enable or disable image protection').setRequired(true))
-            .addIntegerOption(opt => opt.setName('max_amount').setDescription('Max images allowed per message').setRequired(false))
-            .addIntegerOption(opt => opt.setName('timeout').setDescription('Timeout duration in minutes').setRequired(false))
-        )
-        .addSubcommand(subcommand =>
-            subcommand.setName('logs')
-            .setDescription('Set the channel where protection logs are sent')
-            .addChannelOption(opt => opt.setName('channel').setDescription('The channel to send logs to').setRequired(true))
-        )
 ].map(command => command.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
