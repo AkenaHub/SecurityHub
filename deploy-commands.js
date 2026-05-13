bd16977609ee6df5e2f5a689c023245f17dd0898
Akena <hubforroblox@gmail.com>
2026-05-13 13:33:51 -0400

    Remove console logs for command refresh

--- deploy-commands.js
+++ deploy-commands.js
@@ -11,14 +11,11 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
     try {
-        console.log('Started refreshing application (/) commands.');
-
         await rest.put(
             Routes.applicationCommands(process.env.DISCORD_CLIENT_ID),
             { body: commands },
         );
-
-        console.log('Successfully reloaded application (/) commands.');
+        console.log('Successfully reloaded /setup command.');
     } catch (error) {
         console.error(error);
     }
