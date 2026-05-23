6284ec9102a6cbd39d871182a3dc4adf730302dd
Akena <hubforroblox@gmail.com>
2026-05-23 11:56:20 -0400

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -21,6 +21,25 @@ const commands = [
         .setName('dashboard')
         .setDescription('Get the link to the ServSecurity web control panel.')
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    new SlashCommandBuilder()
+        .setName('kick')
+        .setDescription('Kick a user from the server.')
+        .addUserOption(option => option.setName('target').setDescription('The user to kick').setRequired(true))
+        .addStringOption(option => option.setName('reason').setDescription('Reason for the kick').setRequired(false))
+        .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
+    new SlashCommandBuilder()
+        .setName('ban')
+        .setDescription('Ban a user from the server.')
+        .addUserOption(option => option.setName('target').setDescription('The user to ban').setRequired(true))
+        .addStringOption(option => option.setName('reason').setDescription('Reason for the ban').setRequired(false))
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+    new SlashCommandBuilder()
+        .setName('timeout')
+        .setDescription('Timeout a user for a specific duration.')
+        .addUserOption(option => option.setName('target').setDescription('The user to timeout').setRequired(true))
+        .addIntegerOption(option => option.setName('duration').setDescription('Duration in minutes').setRequired(true))
+        .addStringOption(option => option.setName('reason').setDescription('Reason for the timeout').setRequired(false))
+        .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
 ].map(command => command.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(token);
@@ -32,7 +51,7 @@ const rest = new REST({ version: '10' }).setToken(token);
             Routes.applicationCommands(clientId),
             { body: commands },
         );
-        console.log('Successfully loaded the /dashboard command globally.');
+        console.log('Successfully loaded moderation commands globally.');
     } catch (error) {
         console.error('Failed to deploy commands:', error.message);
     }
